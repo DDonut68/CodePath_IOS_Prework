34a300dfb81353d9efa5d6fb4268abc948f2ae8e
Brian Do <99933620+DDonut68@users.noreply.github.com>
2023-01-06 14:10:57 -0500

    IOS app uploaded

---
+++ CodePath_Prework/CodePath_Prework/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  CodePath_Prework
+//
+//  Created by DDonut on 1/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
+    
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!).\nI am currently a \(year!) year student at \(schoolNameTextField.text!).\nAt home, I have \(numberOfPetsLabel.text!) pets, and it is \(morePetsSwitch.isOn) that I want more pets."
+                
+//        print(introduction)
+        
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Nice to meet you!!!", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
